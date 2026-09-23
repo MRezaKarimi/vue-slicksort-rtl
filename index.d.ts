198399261fd998f276b16a76505651e0a0ac980a
MR Karimi <mr.karimi77@yahoo.com>
2026-09-23 09:28:45 +0330

    fix: TS errors on import

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,14 @@
-declare module 'vue-slicksort' {
-  import { DirectiveOptions, Component, Plugin } from 'vue';
+import { DirectiveOptions, Component, Plugin } from 'vue';
 
-  export const ContainerMixin: Component;
-  export const ElementMixin: Component;
+export const ContainerMixin: Component;
+export const ElementMixin: Component;
 
-  export const SlickList: Component;
-  export const SlickItem: Component;
-  export const DragHandle: Component;
+export const SlickList: Component;
+export const SlickItem: Component;
+export const DragHandle: Component;
 
-  export const HandleDirective: DirectiveOptions;
+export const HandleDirective: DirectiveOptions;
 
-  export function arrayMove<T>(arr: Array<T>, prevIndex: number, newIndex: number): Array<T>;
+export function arrayMove<T>(arr: Array<T>, prevIndex: number, newIndex: number): Array<T>;
 
-  export const plugin: Plugin;
-}
+export const plugin: Plugin;
